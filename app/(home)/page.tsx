c849a14d1992f985755b55224c12c167fa43e84d
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-03-10 19:42:30 +0700

    Adding description of finding average

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -44,9 +44,12 @@ export default function Home() {
   useEffect(()=>{
     const arrivesLogs = logs.filter((e)=>e.type === "arrival")
     const departureLogs = logs.filter((e)=>e.type === "departure")
-    
+    //This logic take running time complexity O((n/2)*(n/2)) which O(n**2) if wanted to Access O(1) I think it should change the ways to get data form and change the data structure which I can't done it with in 1 & half hour
     arrivesLogs.map((arrivelog )=>{
       departureLogs.map((departurelog )=>{
+        //Loop looking for and target arrive log and never exist on userMem that tracking is the passengers already calculated
+        // this happend because if we wanted to finding average of all path in database it might have duplicated passenger
+        // running time would be better and easier by changing the way to keep data
         if (arrivelog.passengerName === departurelog.passengerName && !userMem[arrivelog.passengerName]){
             let calTimeUsed = (parseInt(arrivelog.timestamp)*1000) - (parseInt(departurelog.timestamp)*1000)
             const targetPath = `${departurelog.airport}-${arrivelog.airport}`
@@ -88,10 +91,6 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next Airline!</a>
         </h1>
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>app/(home)/page.tsx</code>
-        </p>
 
         <div className={styles.card} style={{ margin: 16, width: "100%" }}>
           <h2>Flight Logs here</h2>
